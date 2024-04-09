0d91962e9519f8ea7c74644a61ae5c103e5abc52
Marginal-Maker <majing350@gmail.com>
2024-04-09 15:53:09 +0800

    feature:添加@Data注解，解决BeanUtils.copyProperties()方法出错的问题。

--- admin/src/main/java/com/majing/shortlink/admin/dto/resp/RealUserRespDto.java
+++ admin/src/main/java/com/majing/shortlink/admin/dto/resp/RealUserRespDto.java
@@ -1,10 +1,13 @@
 package com.majing.shortlink.admin.dto.resp;
 
+import lombok.Data;
+
 /**
  * @author majing
  * @date 2024-04-08 18:37
  * @Description 用户响应参数实体（未脱敏）
  */
+@Data
 public class RealUserRespDto {
     // ID
     private Long id;
--- admin/src/main/java/com/majing/shortlink/admin/dto/resp/UserRespDto.java
+++ admin/src/main/java/com/majing/shortlink/admin/dto/resp/UserRespDto.java
@@ -2,12 +2,14 @@ package com.majing.shortlink.admin.dto.resp;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.majing.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
+import lombok.Data;
 
 /**
  * @author majing
  * @date 2024-04-08 18:37
  * @Description 用户响应参数实体（脱敏）
  */
+@Data
 public class UserRespDto {
     // ID
     private Long id;
