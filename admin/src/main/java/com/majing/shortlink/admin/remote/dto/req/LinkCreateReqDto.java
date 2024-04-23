8df276c3e48e5b1ef7af7114d06a1863bbfa7268
Marginal-Maker <majing350@gmail.com>
2024-04-23 15:06:05 +0800

    feature:前端传递有效期时间序列化

--- admin/src/main/java/com/majing/shortlink/admin/remote/dto/req/LinkCreateReqDto.java
+++ admin/src/main/java/com/majing/shortlink/admin/remote/dto/req/LinkCreateReqDto.java
@@ -1,5 +1,6 @@
 package com.majing.shortlink.admin.remote.dto.req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -22,7 +23,7 @@ public class LinkCreateReqDto {
     private Integer createdType; // 创建类型 0：控制台 1：接口
 
     private Integer validDateType; // 有效期类型 0：永久有效 1：用户自定义
-
+    @JsonFormat(pattern = "yyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate; // 有效期
 
     // @Column(length = 1024) 指定了字段的长度
